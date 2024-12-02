d3e4035f8a63cd0a4724eab31a2349182ff8013d
John <186672498+johntypescode@users.noreply.github.com>
2024-12-02 02:31:55 -0500

    Implementation of functions to retrieve both human and computer game play choices

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 window.onload = function() {
 
-	alert('Hello, world!');
+	// Keep track of each player's score
+	let humanScore = 0;
+	let computerScore = 0;
 
+	// Keep track of the round number
+	let roundNumber = 1;
+
+	// Options that can be played
+	const gameOptions = ['rock', 'paper', 'scissors'];
+
+	/**
+	 * Generate a random choice from the game options for the computer (or, in this case, 'opponent').
+	 */
+
+	function getComputerChoice() {
+		// Select a random choice from the gameOptions array
+		const totalOptions = gameOptions.length;
+		const randomSelection = Math.floor(Math.random() * totalOptions);
+
+		return gameOptions[randomSelection];
+	}
+
+	/**
+	 * Get human user's choices.
+	 */
+	function getHumanChoice() {
+		return prompt('Please select one: rock, paper or scissors...', '');
+	}
 };
